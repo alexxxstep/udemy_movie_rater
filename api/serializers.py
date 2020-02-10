53becf6df434f826aacc8eb49982b040ae3c52e9
alexxxstep <astep2004@gmail.com>
2020-02-11 00:03:57 +0200

    add authorisations

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from .models import Movie, Rating
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'no_of_ratings', 'avr_rating']
+        fields = ('id', 'title', 'description', 'no_of_ratings', 'avr_rating')
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -14,7 +28,7 @@ class MovieSerializer(serializers.ModelSerializer):
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ['id', 'stars', 'user', 'movie']
+        fields = ('id', 'stars', 'user', 'movie')
 
     def create(self, validated_data):
         return Rating.objects.create(**validated_data)
